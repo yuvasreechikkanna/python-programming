a4b7856b40c5bf4cc469674547f2f79d8bbd79b7
yuvasreechikkanna <37336105+yuvasreechikkanna@users.noreply.github.com>
2018-04-13 18:08:45 +0530

    Create array and vowels.py

---
+++ Huntter/array and vowels.py
@@ -0,0 +1,10 @@
+arr=[]
+arr=input()
+def Check_Vow(string, vowels):
+    final = [each for each in string if each in vowels]
+    print("yes")
+     
+
+string = arr
+vowels = "AeEeIiOoUu"
+Check_Vow(string, vowels);
