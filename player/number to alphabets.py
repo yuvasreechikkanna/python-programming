69f28b51832c033c99874792707d3df43e9561d5
yuvasreechikkanna <37336105+yuvasreechikkanna@users.noreply.github.com>
2018-05-01 12:03:24 +0530

    Create number to alphabets.py

---
+++ player/number to alphabets.py
@@ -0,0 +1,6 @@
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+input1=int(input())
+if(input1<26):
+    print (letters[input1])
+else:
+    print("no")
