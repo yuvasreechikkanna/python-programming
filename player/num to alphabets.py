2b1f4ef47e5847402d17417b33d821ddda50135d
yuvasreechikkanna <37336105+yuvasreechikkanna@users.noreply.github.com>
2018-05-01 12:00:24 +0530

    Create num to alphabets.py

---
+++ player/num to alphabets.py
@@ -0,0 +1,3 @@
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+input1=int(input())
+print (letters[input1])
