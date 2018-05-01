e293ccd55b88e882f93fb941e2bf3d278919207f
yuvasreechikkanna <37336105+yuvasreechikkanna@users.noreply.github.com>
2018-05-01 12:10:41 +0530

    Create arithmatic prograssive.py

---
+++ player/arithmatic prograssive.py
@@ -0,0 +1,12 @@
+def sumOfAP( a, d,n) :
+    sum = 0
+    i = 0
+    while i < n :
+        sum = sum + a
+        a = a + d
+        i = i + 1
+    return sum
+n = int(input())
+a = int(input())
+d = int(input())
+print (sumOfAP(a, d, n))
